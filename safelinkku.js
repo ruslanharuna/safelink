175590a59dd6217dbbe81bb80c48ca255aee57b7
ruslanharuna <60423472+ruslanharuna@users.noreply.github.com>
2020-01-29 21:01:46 +0800

    Update safelinkku.js

--- safelinkku.js
+++ safelinkku.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://1001solusifisika.blogspot.com?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://1001solusifisika.blogspot.com/p/safelink.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "n";
